cd0fb4b85f838f452681560b896738b2f45f8426
Jon Heslop <jon@jonheslop.com>
2017-12-18 10:06:37 +0000

    Macro for select box

--- app/index/get.controller.js
+++ app/index/get.controller.js
@@ -1,5 +1,19 @@
 'use strict'
 
 module.exports = (req, res) => {
-  res.render('app/index/get')
+  const params = {
+    countries: [
+      {
+        name: 'United Kingdom',
+        value: 'UK',
+        selected: true
+      },
+      {
+        name: 'France',
+        value: 'FR',
+        selected: false
+      }
+    ]
+  }
+  res.render('app/index/get', params)
 }
