622fdbbbcaa133d9807a8b2e92d580e6e99151de
Danail Minchev <danail.minchev@gmail.com>
2018-04-16 16:41:08 +0100

    PP-3545 Add retrieveGatewayAccount to connectorClient
    - Added GET `/api/accounts/${gatewayAccountId}` endpoint method and `GatewayAccount.class`

---
+++ common/classes/GatewayAccount.class.js
@@ -0,0 +1,16 @@
+'use strict'
+
+class GatewayAccount {
+  constructor (opts) {
+    this.gatewayAccountId = opts.gateway_account_id
+    this.gatewayAccountExternalId = opts.gateway_account_external_id
+    this.paymentMethod = opts.payment_method
+    this.serviceName = opts.service_name
+    this.paymentProvider = opts.payment_provider
+    this.description = opts.description
+    this.type = opts.type
+    this.analyticsId = opts.analytics_id
+  }
+}
+
+module.exports = GatewayAccount
