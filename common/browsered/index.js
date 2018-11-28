b8bd74aaa375fd815792bbf9bfc50dfce90facbf
Jon Heslop <jon@jonheslop.com>
2018-11-28 15:16:53 +0000

    PP-4444 - update base templates to use GOV.UK Frontend

--- common/browsered/index.js
+++ common/browsered/index.js
@@ -4,23 +4,14 @@
 require('./polyfills')
 
 // NPM dependencies
-const $ = window.$ = window.jQuery = require('jquery') // Put this on window for cross compatability
 const analytics = require('gaap-analytics')
-
-// GOV.UK Toolkit dependencies
-require('../../govuk_modules/govuk_frontend_toolkit/javascripts/govuk/show-hide-content')
-require('../../govuk_modules/govuk_frontend_toolkit/javascripts/govuk/stick-at-top-when-scrolling')
+const GOVUKFrontend = require('govuk-frontend') // GOV.UK Frontend js bundle
 
 // Local dependencies
 const fieldValidation = require('./field-validation')
 const inputConfirm = require('./input-confirm')
 
-$(document).ready($ => {
-  const showHideContent = new window.GOVUK.ShowHideContent()
-  showHideContent.init()
-  window.GOVUK.stickAtTopWhenScrolling.init()
-})
-
 fieldValidation.enableFieldValidation()
 inputConfirm()
 analytics.virtualPageview.init()
+GOVUKFrontend.initAll()
