3269311b90d4c4f1a0972deeed22d9b3dc4d16ed
Rory Malcolm <rory.malcolm@digital.cabinet-office.gov.uk>
2019-08-22 13:53:01 +0100

    PP-5511 Change application label for beta
    - Change the application label for the beta of structured logging so
    that there are not conflicts with the lambda

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -1,7 +1,10 @@
 const { createLogger, format, transports } = require('winston')
-const { timestamp, json } = format
+const { timestamp, json, label, splat, prettyPrint } = format
 const logger = createLogger({
   format: format.combine(
+    splat(),
+    label({ label: 'direct-debit-frontend-sl-beta' }),
+    prettyPrint(),
     timestamp(),
     json()
   ),
