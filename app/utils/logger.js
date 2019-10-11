51a0a7af4d99f01bf966bd8961522dbcc4bd57cc
Rory Malcolm <rory.malcolm@digital.cabinet-office.gov.uk>
2019-10-11 10:49:12 +0100

    PP-5511 Change Timestamp Format
    - Changes the format to use '@timestamp' from gds way convention as
    opposed to 'timestamp'

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -1,18 +1,25 @@
 const { createLogger, format, transports } = require('winston')
 const { timestamp, json, label, splat, prettyPrint } = format
+
+const timetstampFormat = format((timestamp) => {
+  return {
+    '@timestamp': timestamp
+  }
+})
+
 const logger = createLogger({
   format: format.combine(
     splat(),
     label({ label: 'direct-debit-frontend-sl-beta' }),
     prettyPrint(),
-    timestamp(),
-    json()
+    json(),
+    timetstampFormat(timestamp())
   ),
   transports: [
     new transports.Console()
   ]
 })
 
-module.exports = ( loggerName ) => {
+module.exports = (loggerName) => {
   return logger.child({ logger: loggerName })
 }
